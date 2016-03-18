57e1cba5d676b66f9cc6a748e5f929d69e85725c
John Bryden <john.bryden@rhul.ac.uk>
2016-03-18 14:36:55 +0000

    First versions

--- powerModel.py
+++ powerModel.py
@@ -1,9 +1,14 @@
+# Copyright John Bryden under GPL3 license 
+#
+# However, don't publish any results generated using this code without
+# my permisison
+
 from pylab import *
 from collections import namedtuple
 import sys
+import argparse
 
-# Right I want a list of nodes and undirectional connections
-
+# this is a unidirectional link between two people
 class Link:
 
     def __init__(self, outPerson, inPerson, linkValueToOut, linkValueToIn):
@@ -12,6 +17,8 @@ class Link:
         self.linkValueToOut = linkValueToOut
         self.linkValueToIn = linkValueToIn
 
+
+# a person (or node) in the network
 class Person:
     
     def __init__ (self,personid):
@@ -21,12 +28,12 @@ class Person:
 
         # the person has a list of its Links to other people
         self.outgoingLinks=list()
-        # they also have a list of Links from others
+        # they also have a dictionary (id -> link) of Links from others
         self.incomingLinks=dict()
 
         # this is the initial status of the person
         self.status=1.0
-        # this is the incoming status
+        # this is to record the status which is incoming from others
         self.incomingStatus = 0
 
     def show (self):
@@ -35,7 +42,7 @@ class Person:
         print "Incoming status is",self.incomingStatus
         for link in self.outgoingLinks:
             print "->",link.inPerson.personid
-        print len(self.incomingLinks),"incoming links"
+        print self.numIncomingLinks,"incoming links"
         print
 
     def getNumLinks(self):
@@ -45,61 +52,47 @@ class Person:
         status += statusChange
         statusChange = 0
 
-    def getWorstLink(self):
+    # This function calculates the outgoing link which is of least
+    # value to the individual
+    def getWorstLink(self, addIncomingValueIfLinkIsMutual=False):
         worstlink = None
         worstValue= 1e500
         for link in self.outgoingLinks:
-            if link.linkValueToOut < worstValue:
+            linkvalue = link.linkValueToOut
+            if addIncomingValueIfLinkIsMutual:
+                if link.inPerson.personid in self.incomingLinks:
+                    linkvalue += self.incomingLinks[link.inPerson.personid].linkValueToIn
+            if linkvalue < worstValue:
                 worstValue = link.linkValueToOut
                 worstlink = link
 
         return worstlink
 
-#    def addOutgoingLink(self,linkedPerson):
-#        outgoingLinks.append(Link(linkedPerson=linkedPerson,linkValue=0.0))
-
-#    def addIncomingLink(self,linkedPerson):
-#        outgoingLinks.append(Link(linkedPerson=linkedPerson,linkValue=0.0))
-        
-
-
-# How does link value work?
-
-# The whole things starts with numpeople status 'points' at the
-# beginning.  That should be conserved.
-#
-# In total there are numpeople*numLinks*2 actual Links
-
-# The link's status is person1Status/person1TotalLinks +
-# person2/person2TotalLinks
-
-# With an inequality factor of q, the person with the link incoming
-# gets q of the shared status.
-
 
 class Population:
-    # a dictionary from id to Person
+    # Manages a dictionary (id->person) of people and a list of links
+    # between them
     
-    numPeople = 20
-    numLinks  = 5
-    
-    # this is essentially a C struct
 
-    def __init__(self):
+    def __init__(self, numPeople,numLinks,r=0.2,q=0.9):
 
-        self.q = 0.9
-        self.r = 0.2
-        self.w = 1.0
+        self.numPeople = 20
+        self.numLinks  = 5
+
+
+        self.q = q
+        self.r = r
 
         self.people = dict()
         self.links = list()
 
-        #        self.numlinksvsstatus_output=open('numlinksvsstatus.dat','w')
+        # to record some data which will later be plotted
         self.numlinksvsstatus=list()
 
         for i in range (0,self.numPeople):
             self.people[i] = Person(i)
 
+        # generate a random network
         for pid,person in self.people.items():
             avoidPeople=[person.personid,]
             for j in range (0,self.numLinks):
@@ -111,9 +104,10 @@ class Population:
                 linkedPerson.incomingLinks[person.personid]=newlink
 
 
+    # functions for getting random individuals
     def getRandomPerson (self):
         return self.people[randint(self.numPeople)]
-                
+
     def findIndividualToLinkTo (self, avoidPeopleIDs):
         newPerson = self.getRandomPerson()
         while newPerson.personid in avoidPeopleIDs:
@@ -121,23 +115,25 @@ class Population:
 
         return newPerson
         
-        
 
     def showPeople(self):
         for pid,person in self.people.items():
             person.show()
 
     def updateStatuses(self):
+        # reset link values
         for link in self.links:
             link.linkValueToIn = 0.0
             link.linkValueToOut = 0.0
+        
         
         for link in self.links:
             outPerson = link.outPerson
             inPerson = link.inPerson
 
-            # Each person gives a proportion r of their status and
-            # divides that amongst their links.
+            # Each person takes a proportion r of their status (which
+            # will be deducted later) and divides that amongst their
+            # links.
             outStatusForLink = self.r*outPerson.status/float(outPerson.getNumLinks())
             inStatusForLInk = self.r*inPerson.status/float(inPerson.getNumLinks())
          
@@ -173,29 +169,40 @@ class Population:
     def rewireLinks (self):
 
         for pid,person in self.people.items():
-            if rand() < self.w:
-#                print "Person",person.personid,"rewiring from",
-                worstLink = person.getWorstLink()
-                oldPerson = worstLink.inPerson
-                oldPerson.incomingLinks.pop(person.personid)
-                #                print oldPerson.personid,
+            #                print "Person",person.personid,"rewiring from",
 
-                avoidPeople = [person.personid,]
-                for link in person.outgoingLinks:
-                    avoidPeople.append(link.inPerson.personid)
+            worstLink = person.getWorstLink()
+            personBeingRemoved = worstLink.inPerson
+            #                print personBeingRemoved.personid,
 
-                newPerson = self.findIndividualToLinkTo(avoidPeople)
+            # Remove this link from the old person's links
+            personBeingRemoved.incomingLinks.pop(pid)
 
-                worstLink.inPerson = newPerson
-                newPerson.incomingLinks[person.personid]=worstLink
-                if len(newPerson.incomingLinks) == self.numPeople:
-                    print person.personid
-                    print avoidPeople
-                    print newPerson.personid
-                    print oldPerson.personid
-                    print worstLink.outPerson.personid, worstLink.inPerson.personid
-#               print "to",newPerson.personid
+            # In this version, it won't rewire to any of those
+            # already linked to (including the person being
+            # removed)
+            avoidPeople = [person.personid,]
+            for link in person.outgoingLinks:
+                avoidPeople.append(link.inPerson.personid)
 
+            newPerson = self.findIndividualToLinkTo(avoidPeople)
+
+            #update the link and give it to the new person
+            worstLink.inPerson = newPerson
+
+            newPerson.incomingLinks[person.personid]=worstLink
+
+            # this checks if there is a bug where an invidual has
+            # too many incoming linsk
+            if len(newPerson.incomingLinks) == self.numPeople:
+                print person.personid
+                print avoidPeople
+                print newPerson.personid
+                print personBeingRemoved.personid
+                print worstLink.outPerson.personid, worstLink.inPerson.personid
+                #               print "to",newPerson.personid
+
+    # for debugging purposes
     def findAnomalousIndividual (self):
         for pid,person in self.people.items():
             if person.getNumLinks() > (self.numLinks+self.numPeople-1):
@@ -206,22 +213,28 @@ class Population:
 
                         
 
-
-
 print sys.argv
 
-qval = 0.9
+parser = argparse.ArgumentParser()
+parser.add_argument ('-q',type=float,default=0.5, help="q: level of inequality in the model")
+parser.add_argument ('-r',type=float,default=0.2, help="r: rate that status is contributed to others")
+parser.add_argument ('-n',type=int,default=20, help="number of people")
+parser.add_argument ('-l',type=int,default=5, help="number of links")
+parser.add_argument ('--show',action="store_true", help="Show graph")
 
-if len(sys.argv)>1:
-    qval = float(sys.argv[1])
+args = parser.parse_args()
 
+print args
 
-print "q = ",qval
- 
+qval = float(args.q)
+rval = float(args.r)
+nval = int(args.n)
+lval = int(args.l)
+
+population = Population(nval,lval,rval,qval)
+
 data=[]
-           
-population = Population()
-population.q = qval
+
 for t in range(0,100000):
     population.updateStatuses()
     population.rewireLinks()
@@ -243,8 +256,9 @@ lvs=array(population.numlinksvsstatus)
 plot (lvs[:,0],lvs[:,1],'o',alpha=0.01, markersize=30)
 xlabel('Number of links')
 ylabel('Status')
-savefig(str(qval)+'_fig.png')
-#show()
+savefig('q_'+str(qval)+'_r_'+str(rval)+'_n_'+str(nval)+'_l_'+str(lval)+'_fig.png')
+if args.show:
+    show()
 
 
 
